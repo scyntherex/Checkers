2e535b0814232948bef8d8f83ccc398fb2cdd860
Rone <scyntherex@Rones-MacBook-Pro.local>
2017-10-23 10:47:52 -0700

    Added Checker, CheckerType, and edited main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,32 @@
+import java.awt.EventQueue;
 
-public class Main {
+import javax.swing.JFrame;
 
+public class Main extends JFrame{
+	
+	public Main(String title) {
+	      super(title);
+	      setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	      Board board = new Board();
+	      board.add(new Checker(CheckerType.RED_REGULAR), 4, 1);
+	      board.add(new Checker(CheckerType.BLACK_REGULAR), 6, 3);
+	      board.add(new Checker(CheckerType.RED_KING), 5, 6);
+	      setContentPane(board);
+
+	      pack();
+	      setVisible(true);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		Runnable r = new Runnable()
+        {
+           @Override
+           public void run()
+           {
+              new Main("Checkers");
+           }
+        };
+        EventQueue.invokeLater(r);
 	}
 
 }
